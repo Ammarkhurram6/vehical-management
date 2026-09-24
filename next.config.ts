dc7314c06a896cc24e4477c4ec7e57b76c53afd1
Ammarkhurram6 <ammarkhuram6@gmail.com>
2026-09-24 21:28:05 +0500

    update global.css

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  reactStrictMode: false,
+  // Simply set to false to completely hide the development indicator
+  devIndicators: false,
 };
 
 export default nextConfig;
